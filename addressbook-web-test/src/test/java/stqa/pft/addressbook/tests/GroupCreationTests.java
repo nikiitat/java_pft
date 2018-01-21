1349edaaafe22abfd98730bd93e1490dcc36a43c
Nikita <nikita.tertytskyi@gmail.com>
2018-01-21 12:37:35 +0100

    test

--- addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("chrome", null, null));
+        app.getGroupHelper().fillGroupForm(new GroupData("test", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
