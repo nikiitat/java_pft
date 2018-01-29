cab25fbaefc0eb13993404033b752bd6374c2b5c
Nikita <nikita.tertytskyi@gmail.com>
2018-01-29 21:18:34 +0100

    Verification and enforcement of preconditions

--- addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
--- addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -9,8 +9,11 @@ import stqa.pft.addressbook.model.GroupData;
 public class GroupModificationTests extends TestBase {
 
     @Test
-    public void testGroupModification(){
+    public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("tratata", "tratata", "tratata"));
