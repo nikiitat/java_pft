1e0a7098ed99fd1b741443c21a44b8b24fe81eb2
Nikita <nikita.tertytskyi@gmail.com>
2018-02-01 16:03:15 +0100

    disable contact tests

--- addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
+    @Test(enabled = false)
     public void testContactCreation() {
         List<ContactData> before = app.getContactHelper().getContactList();
         ContactData contact = new ContactData("Test123", "Tester", "book",
