7470d4e11e75a6e4e0c20c0ab8e814655b1e3c4b
Nikita <nikita.tertytskyi@gmail.com>
2018-02-08 21:14:04 +0100

    create a Contact generator

---
+++ addressbook-web-test/src/test/java/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,64 @@
+package stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nikitatertytskyi on 08.02.2018.
+ */
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contact = generateContacts(count);
+        save(contact, new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s\n", contact.getFirstName(),
+                    contact.getLastName(), contact.getEmail(), contact.getHomePhone()));
+        }
+        writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstName(String.format("test %s", i))
+                    .withLastName(String.format("test %s", i))
+                    .withEmail(String.format("test%s@test.com", i))
+                    .withHomePhone(String.format("+1%s34567890", i)));
+        }
+        return contacts;
+    }
+}
