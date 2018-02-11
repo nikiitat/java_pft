4887c0fdcce08c31f09e5c301be434305e67eee3
Nikita <nikita.tertytskyi@gmail.com>
2018-02-11 17:52:45 +0100

    add logger logBack

--- addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,23 @@
 package stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by nikitatertytskyi on 28.12.17.
  */
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static final ApplicationManager app =
             new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -18,7 +26,17 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
+
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
