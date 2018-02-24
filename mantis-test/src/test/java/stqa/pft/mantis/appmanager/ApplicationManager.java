d90a825832ecb805609ddfedc0413ecec514469a
Nikita <nikita.tertytskyi@gmail.com>
2018-02-24 12:52:11 +0100

    add modile for mantis tests

---
+++ mantis-test/src/test/java/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,51 @@
+package stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by nikitatertytskyi on 28.12.17.
+ */
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("targer", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty(properties.getProperty("driver.firefox"), properties.getProperty("firefox2"));
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.SAFARI)) {
+            wd = new SafariDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+}
---
+++ mantis-test/src/test/java/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,26 @@
+package stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import stqa.pft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by nikitatertytskyi on 28.12.17.
+ */
+public class TestBase {
+
+    protected static final ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
