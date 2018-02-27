33e5fd3743fea61491902fe80fa6902092fc4d2e
Nikita <nikita.tertytskyi@gmail.com>
2018-02-27 01:08:15 +0100

    add test for ftp

--- mantis-test/src/test/java/stqa/pft/mantis/appmanager/FtpHelper.java
+++ mantis-test/src/test/java/stqa/pft/mantis/appmanager/FtpHelper.java
@@ -29,7 +29,7 @@ public class FtpHelper {
         ftp.disconnect();
     }
 
-    public void restore(File file, String target, String backup) throws IOException {
+    public void restore(String backup, String target) throws IOException {
         ftp.connect(app.getProperty("ftp.host"));
         ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
         ftp.deleteFile(target);
--- mantis-test/src/test/java/stqa/pft/mantis/tests/TestBase.java
+++ mantis-test/src/test/java/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by nikitatertytskyi on 28.12.17.
  */
@@ -16,10 +19,12 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftpHelper().upload(new File("src/test/resources/config_inc.php"), "mantisbt-1.2.19/config_inc.php", "mantisbt-1.2.19/config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        app.ftpHelper().restore("mantisbt-1.2.19/config_inc.php.bak", "mantisbt-1.2.19/config_inc.php");
         app.stop();
     }
 
