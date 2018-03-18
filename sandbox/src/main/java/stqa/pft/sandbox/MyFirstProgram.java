11ffd610c251263ee8e4a49a36f35c2250c185a4
Nikita <nikita.tertytskyi@gmail.com>
2018-03-18 22:48:07 +0100

    continue bubble sort

--- sandbox/src/main/java/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package stqa.pft.sandbox;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MyFirstProgram {
 
@@ -10,15 +10,19 @@ public class MyFirstProgram {
 
         int[] numbers = {10, 6, 15, 4, 8, 20, 2};
         int a = 27;
-        List<Integer> sum = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+//        List<int[]> matrix = new ArrayList<>();
 
-        for (int i = 0; i < numbers.length - 1; i++) {
+        System.out.println(matrix.toString());
+        for (int i = 0; i < numbers.length; i++) {
             for (int j = 1; j < numbers.length; j++) {
-                sum.add(numbers[i] + numbers[j]);
-                System.out.println(sum);
+                ArrayList<Integer> row = new ArrayList<Integer>(Arrays
+                        .asList(numbers[i] + numbers[j], numbers[i], numbers[j]));
+                matrix.add(row);
             }
         }
-//        findSum();
+        System.out.println(matrix.toString());
+        findSum(matrix, a);
 
 //        Point p1 = new Point(1, 3);
 //        Point p2 = new Point(4, 2);
@@ -27,14 +31,32 @@ public class MyFirstProgram {
 //        System.out.println("Calling a method: Distance between p2 and p1 =  " + p2.distance(p1));
     }
 
-    public static int findSum(List<Integer> sum, int a) {
-        for (int c : sum) {
-            int i = a - c;
-            if (i == 0) {
-                return i;
+    private static List<Integer> findSum(ArrayList<ArrayList<Integer>> matrix, int a) {
+        List<List<Integer>> m = new ArrayList<>();
+        List<List<Integer>> p = new ArrayList<>();
+        for (List<Integer> list : matrix) {
+            if (list.get(0).equals(a)) {
+                return list;
+            } else if (list.get(0) - a < 0) {
+                m.add(list);
+            } else if (list.get(0) - a > 0) {
+                p.add(list);
             }
         }
-        return a;
+        System.out.println(p.toString());
+        int max = m.stream().mapToInt((g) -> g.get(0)).max().getAsInt();
+        int min = p.stream().mapToInt((g) -> g.get(0)).min().getAsInt();
+        System.out.println(min + "sss");
+        System.out.println(max + "sss");
+        List<List<Integer>> collect = p.stream().filter((g) -> g.get(0).equals(min)).collect(Collectors.toList());
+        List<List<Integer>> collect1 = m.stream().filter((g) -> g.get(0).equals(max)).collect(Collectors.toList());
+
+
+        List<List<Integer>> collect2 = new ArrayList<>();
+        collect2.add(collect.get(0));
+        collect2.add(collect1.get(0));
+        System.out.println(collect.get(0) + "+" + collect1.get(0));
+        return collect2.get(0);
     }
 
     public static double distance(Point p1, Point p2) {
